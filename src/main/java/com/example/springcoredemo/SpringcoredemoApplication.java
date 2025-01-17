4bb38b34d3e93a16be824707d4593edecd0bb215
hari0409 <hari626007@gmail.com>
2025-01-18 01:16:31 +0530

    Clean Up for Hibernate-JPA

--- src/main/java/com/example/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/example/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,7 @@ package com.example.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springcoredemo/common/SwimCoach.java
+++
@@ -1,8 +0,0 @@
-package com.example.springcoredemo.common;
-
-public class SwimCoach implements Coach {
-    @Override
-    public String getDailyWorkout() {
-        return "100 meter in 2 mins";
-    }
-}
