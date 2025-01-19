e62187f3385d747c6fb89e969a61c494cceed527
hari0409 <hari626007@gmail.com>
2025-01-19 16:54:24 +0530

    Cleanup for REST Crud

--- src/main/java/com/example/demo_crud/DemoCrudApplication.java
+++ src/main/java/com/example/demo_crud/DemoCrudApplication.java
@@ -1,39 +1,12 @@
 package com.example.demo_crud;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.demo_crud.dao.StudentDao;
-import com.example.demo_crud.entity.Student;
 
 @SpringBootApplication
 public class DemoCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCrudApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(StudentDao studentDao) {
-		return runner -> {
-			createStudent(studentDao);
-		};
-	}
-
-	private void createStudent(StudentDao studentDao) {
-		Student s = new Student(
-				UUID.randomUUID().toString().substring(0, 10).toUpperCase(),
-				UUID.randomUUID().toString().substring(0, 5).toUpperCase(),
-				UUID.randomUUID().toString().substring(0, 15) + "@gmail.com");
-		System.out.println(UUID.randomUUID().toString());
-		studentDao.save(s);
-		System.out.println(s.getId());
 	}
 }
