354e1ff1e86e812bb0fea72163ef8f27593800c7
hari0409 <hari626007@gmail.com>
2025-01-27 01:44:25 +0530

    Spring Security Setup

--- src/main/java/com/example/crud_demo/constants/ProjectConstant.java
+++
@@ -1,6 +0,0 @@
-package com.example.crud_demo.constants;
-
-public class ProjectConstant {
-    public static final String SUCCESS = "success";
-    public static final String FAILURE = "failure";
-}
