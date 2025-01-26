6938fa5814fa2d304e65ce7bc3089f91830e39f7
hari0409 <hari626007@gmail.com>
2025-01-26 20:37:21 +0530

    Spring Data Rest

--- src/main/java/com/example/crud_demo/jpaRepository/EmployeeRepository.java
+++ src/main/java/com/example/crud_demo/jpaRepository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.example.crud_demo.jpaRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.crud_demo.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
+
 }
