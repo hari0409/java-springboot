fc829f83dff15d26a6b3459b7bca0fdac0b99516
hari0409 <hari626007@gmail.com>
2025-02-01 00:27:41 +0530

    Advanced Hibernate Clean Up

--- src/main/java/com/demo/springboot/cruddemo/dao/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.springboot.cruddemo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.springboot.cruddemo.entity.Members;
-
-public interface MemberRepository extends JpaRepository<Members, String> {
-
-}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.demo.springboot.cruddemo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
